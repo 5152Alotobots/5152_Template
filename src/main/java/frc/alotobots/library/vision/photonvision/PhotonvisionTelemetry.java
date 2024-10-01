3a2d0a9ef345c178b7fbde5922cc2165ca65ee34
SeanErn <sean.ernstes@proton.me>
2024-09-30 23:16:20 -0400

    feat: Add PhotonvisionTelemetry class

--- src/main/java/frc/alotobots/library/vision/photonvision/PhotonvisionTelemetry.java
+++ src/main/java/frc/alotobots/library/vision/photonvision/PhotonvisionTelemetry.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 /** Handles telemetry for the Photonvision subsystem. */
