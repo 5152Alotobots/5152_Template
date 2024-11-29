0ad17f2252b2a6797e24035f9f831191f4ef3246
SeanErn <sean.ernstes@proton.me>
2024-11-28 19:51:20 -0500

    Tuned PIDF for 2022 drive

--- src/main/java/frc/alotobots/library/drivetrains/swerve/ctre/mk4il22023/TunerConstants.java
+++ src/main/java/frc/alotobots/library/drivetrains/swerve/ctre/mk4il22023/TunerConstants.java
@@ -26,16 +26,16 @@ public class TunerConstants {
   // Both sets of gains need to be tuned to your individual robot.
   private static final Slot0Configs steerGains =
       new Slot0Configs()
-          .withKP(0)
+          .withKP(55)
           .withKI(0)
-          .withKD(0)
+          .withKD(0.2)
           .withKS(0.13)
-          .withKV(0)
-          .withKA(0)
+          .withKV(0.102)
+          .withKA(0.015)
           .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
 
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(3).withKI(0).withKD(0).withKS(0).withKV(0);
+      new Slot0Configs().withKP(5.5).withKI(0).withKD(0).withKS(0.13).withKV(0.99).withKA(0);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
