f6775f70f9381e285def2a3a48f08c7d4b4520c2
SeanErn <sean.ernstes@proton.me>
2024-12-08 01:16:06 -0500

    feat: Add PhoenixOdometryThread for asynchronous odometry signal reading

---
+++ src/main/java/frc/alotobots/library/subsystems/swervedrive/PhoenixOdometryThread.java
@@ -0,0 +1,158 @@
+//   ____  _ ____ ____       _    _        ___ _____ ___  ____     ___ _____ ____
+//  | ___|/ | ___|___ \    / \  | |     / _ \_   _/ _ \| __) / _ \_   _/ ___|
+//  |___ \| |___ \ __) | / _ \ | |    | | | || || | | |  _ \|| | || | \___ \
+//   ___) | |___) / __/   / ___ \| |__  | |_| || || |_| | |_) | |_| || |  ___) |
+//  |____/|_|____/_____| /_/   \_\_____\___/ |_| \___/|____/ \___/ |_| |____/
+//
+//
+// 2025 ALOTOBOTS FRC 5152
+// Robot Code
+package frc.alotobots.library.subsystems.swervedrive;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.RobotController;
+import frc.alotobots.library.subsystems.swervedrive.constants.TunerConstants;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.DoubleSupplier;
+
+/**
+ * Provides an interface for asynchronously reading high-frequency measurements to a set of queues.
+ */
+public class PhoenixOdometryThread extends Thread {
+  private final Lock signalsLock = new ReentrantLock();
+  private BaseStatusSignal[] phoenixSignals = new BaseStatusSignal[0];
+  private final List<DoubleSupplier> genericSignals = new ArrayList<>();
+  private final List<Queue<Double>> phoenixQueues = new ArrayList<>();
+  private final List<Queue<Double>> genericQueues = new ArrayList<>();
+  private final List<Queue<Double>> timestampQueues = new ArrayList<>();
+
+  private final TunerConstants tunerConstants;
+  private static PhoenixOdometryThread instance = null;
+  private final boolean isCANFD;
+
+  public static PhoenixOdometryThread initialize(TunerConstants constants) {
+    if (instance == null) {
+      instance = new PhoenixOdometryThread(constants);
+    }
+    return instance;
+  }
+
+  public static PhoenixOdometryThread getInstance() {
+    if (instance == null) {
+      throw new IllegalStateException(
+          "PhoenixOdometryThread must be initialized with constants first");
+    }
+    return instance;
+  }
+
+  private PhoenixOdometryThread(TunerConstants constants) {
+    this.tunerConstants = constants;
+    this.isCANFD = new CANBus(constants.getDrivetrainConstants().CANBusName).isNetworkFD();
+    setName("PhoenixOdometryThread");
+    setDaemon(true);
+  }
+
+  @Override
+  public void start() {
+    if (timestampQueues.size() > 0) {
+      super.start();
+    }
+  }
+
+  public Queue<Double> registerSignal(StatusSignal<Angle> signal) {
+    Queue<Double> queue = new ArrayBlockingQueue<>(20);
+    signalsLock.lock();
+    SwerveDriveSubsystem.odometryLock.lock();
+    try {
+      BaseStatusSignal[] newSignals = new BaseStatusSignal[phoenixSignals.length + 1];
+      System.arraycopy(phoenixSignals, 0, newSignals, 0, phoenixSignals.length);
+      newSignals[phoenixSignals.length] = signal;
+      phoenixSignals = newSignals;
+      phoenixQueues.add(queue);
+    } finally {
+      signalsLock.unlock();
+      SwerveDriveSubsystem.odometryLock.unlock();
+    }
+    return queue;
+  }
+
+  public Queue<Double> registerSignal(DoubleSupplier signal) {
+    Queue<Double> queue = new ArrayBlockingQueue<>(20);
+    signalsLock.lock();
+    SwerveDriveSubsystem.odometryLock.lock();
+    try {
+      genericSignals.add(signal);
+      genericQueues.add(queue);
+    } finally {
+      signalsLock.unlock();
+      SwerveDriveSubsystem.odometryLock.unlock();
+    }
+    return queue;
+  }
+
+  public Queue<Double> makeTimestampQueue() {
+    Queue<Double> queue = new ArrayBlockingQueue<>(20);
+    SwerveDriveSubsystem.odometryLock.lock();
+    try {
+      timestampQueues.add(queue);
+    } finally {
+      SwerveDriveSubsystem.odometryLock.unlock();
+    }
+    return queue;
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      // Wait for updates from all signals
+      signalsLock.lock();
+      try {
+        if (isCANFD && phoenixSignals.length > 0) {
+          BaseStatusSignal.waitForAll(2.0 / tunerConstants.getOdometryFrequency(), phoenixSignals);
+        } else {
+          Thread.sleep((long) (1000.0 / tunerConstants.getOdometryFrequency()));
+          if (phoenixSignals.length > 0) BaseStatusSignal.refreshAll(phoenixSignals);
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } finally {
+        signalsLock.unlock();
+      }
+
+      // Save new data to queues
+      SwerveDriveSubsystem.odometryLock.lock();
+      try {
+        // Sample timestamp is current FPGA time minus average CAN latency
+        double timestamp = RobotController.getFPGATime() / 1e6;
+        double totalLatency = 0.0;
+        for (BaseStatusSignal signal : phoenixSignals) {
+          totalLatency += signal.getTimestamp().getLatency();
+        }
+        if (phoenixSignals.length > 0) {
+          timestamp -= totalLatency / phoenixSignals.length;
+        }
+
+        // Add new samples to queues
+        for (int i = 0; i < phoenixSignals.length; i++) {
+          phoenixQueues.get(i).offer(phoenixSignals[i].getValueAsDouble());
+        }
+        for (int i = 0; i < genericSignals.size(); i++) {
+          genericQueues.get(i).offer(genericSignals.get(i).getAsDouble());
+        }
+        for (int i = 0; i < timestampQueues.size(); i++) {
+          timestampQueues.get(i).offer(timestamp);
+        }
+      } finally {
+        SwerveDriveSubsystem.odometryLock.unlock();
+      }
+    }
+  }
+}
