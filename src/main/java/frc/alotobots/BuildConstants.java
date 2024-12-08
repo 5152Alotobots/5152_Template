50ce598362bc938f6ca82098e302b71b2e64d304
SeanErn <sean.ernstes@proton.me>
2024-12-07 22:50:00 -0500

    master only docs

---
+++ src/main/java/frc/alotobots/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.alotobots;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "5152_Template";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 240;
+  public static final String GIT_SHA = "59a84af681ba0c9a79317a4189d4775696c10f3e";
+  public static final String GIT_DATE = "2024-12-07 22:29:54 EST";
+  public static final String GIT_BRANCH = "AkitFresh";
+  public static final String BUILD_DATE = "2024-12-07 22:30:00 EST";
+  public static final long BUILD_UNIX_TIME = 1733628600746L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
