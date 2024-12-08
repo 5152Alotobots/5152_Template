76bd0862cc4a9e877bb25456799ee463b86b9e9f
SeanErn <sean.ernstes@proton.me>
2024-12-08 02:03:09 -0500

    feat: Add OI class with driver controller configuration

---
+++ src/main/java/frc/alotobots/OI.java
@@ -0,0 +1,13 @@
+package frc.alotobots;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class OI {
+
+    // Controllers
+    private static final CommandXboxController driverController = new CommandXboxController(0);
+
+    
+}
