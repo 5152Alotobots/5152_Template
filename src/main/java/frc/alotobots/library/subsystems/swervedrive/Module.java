0a4e92f6eecc1c0a01c3a3369ea5d3bb1d1801d1
SeanErn <sean.ernstes@proton.me>
2024-12-21 19:25:38 -0500

    refactor: Comment out characterization method for MOI estimation

--- src/main/java/frc/alotobots/library/subsystems/swervedrive/Module.java
+++ src/main/java/frc/alotobots/library/subsystems/swervedrive/Module.java
@@ -86,6 +86,11 @@ public class Module {
     io.setDriveOpenLoop(output);
     io.setTurnPosition(new Rotation2d());
   }
+// Replace this to estimate MOI via SysID
+//  public void runCharacterization(double output) {
+//    io.setDriveOpenLoop(output);
+//    io.setTurnPosition(new Rotation2d(constants.LocationX, constants.LocationY).plus(Rotation2d.kCCW_Pi_2));
+//}
 
   /** Disables all outputs to motors. */
   public void stop() {
